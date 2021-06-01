04f87d3507c05f1adfe75735d92487a270beefab
doctor-design <71015256+doctor-design@users.noreply.github.com>
2021-06-01 17:20:26 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -65,7 +65,7 @@ if [ -z $TYPES ];then
     TYPES="n1"
 fi
 if [ -z $OPENWRT_VERSION ];then
-	OPENWRT_VERSION="R21.6.1"
+	OPENWRT_VERSION="R21.4.18"
 fi
 if [ -z $KERNEL_VERSION ];then
     KERNEL_VERSION="Latest"
