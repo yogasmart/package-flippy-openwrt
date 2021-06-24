2b9caf5e1fc46894872cd0ef087f0c8f29355dd1
yogasmart <72687945+yogasmart@users.noreply.github.com>
2021-06-24 16:48:41 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -65,7 +65,7 @@ if [ -z $TYPES ];then
     TYPES="n1"
 fi
 if [ -z $OPENWRT_VERSION ];then
-	OPENWRT_VERSION="R21.4.18"
+	OPENWRT_VERSION="R21.6.22"
 fi
 if [ -z $KERNEL_VERSION ];then
     KERNEL_VERSION="Latest"
